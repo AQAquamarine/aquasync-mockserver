977cd1ee177352976cb30d4bbda21263c3d77fa7
Kai INUI <lied.der.optik@gmail.com>
2014-07-30 13:45:42 +0900

    add required attributes

--- models/base.rb
+++ models/base.rb
@@ -9,6 +9,11 @@ module Aquasync
       include Mongoid::Document
 
       field :ust, type: Integer
+      field :isDirty, type: Boolean
+      field :localTimestamp, type: DateTime
+      field :gid, type: String
+      field :deviceToken, type: String
+      field :isDeleted, type: Boolean
 
       before_save do
         set_ust
