e9b7dfbf9fd01a93490d4d1f6542ba08885c4eab
Kai INUI <lied.der.optik@gmail.com>
2014-07-30 13:52:35 +0900

    force gid to be lowercase and validation

--- models/base.rb
+++ models/base.rb
@@ -15,8 +15,16 @@ module Aquasync
       field :deviceToken, type: String
       field :isDeleted, type: Boolean
 
+      # UUID like 550e8400-e29b-41d4-a716-446655440000
+      validates :gid, format: { with: /^([0-9abcdef].*-?)$/}
+
       before_save do
+        downcase_gid
         set_ust
+      end
+
+      def downcase_gid
+        self.gid.downcase!
       end
 
       def set_ust
