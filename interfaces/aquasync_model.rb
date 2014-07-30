680f27b7c737192c32ecd7a546a4b9df639fc944
Kai INUI <lied.der.optik@gmail.com>
2014-07-31 02:48:07 +0900

    delete interfacs

--- interfaces/aquasync_model.rb
+++
@@ -1,9 +0,0 @@
-class AquasyncModel
-  def aq_deltas
-    raise NotImplementedError, "Implement this method in a child class."
-  end
-
-  def aq_commit_deltas
-    raise NotImplementedError, "Implement this method in a child class."
-  end
-end
