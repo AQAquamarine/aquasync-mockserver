313af7222bde6c9b24639583d178f7fbff124971
Kai INUI <lied.der.optik@gmail.com>
2014-07-30 18:22:12 +0900

    implement delta_pack_unpacker

--- lib/delta_pack_unpacker.rb
+++ lib/delta_pack_unpacker.rb
@@ -1,3 +1,12 @@
 class DeltaPackUnpacker
+  attr_accessor :id, :deltas
 
+  def unpack(delta_pack)
+    self.id = delta_pack["_id"]
+    delta_pack.each do |key, value|
+      next if key == "_id"
+      deltas[key] = value
+    end
+    self
+  end
 end
