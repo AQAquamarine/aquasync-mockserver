c944aa6d551ab6f81328f028792251abf1f1d11b
Kai INUI <lied.der.optik@gmail.com>
2014-07-31 18:18:20 +0900

    organize spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,17 +2,21 @@ require 'rspec'
 
 ENV['RACK_ENV'] = "test"
 
-require_relative '../initializer/mongoid'
-require_relative '../lib/delta_pack_builder'
-require_relative 'hoge'
-require_relative 'huga'
-require_relative '../lib/delta_pack_unpacker'
-require_relative '../aggregators/deltas_aggregator'
-require 'factory_girl'
-require_relative 'factories/hoge'
-require_relative 'factories/huga'
 require 'database_cleaner'
 require 'active_support/core_ext'
+require 'factory_girl'
+
+require_relative 'factories/hoge'
+require_relative 'factories/huga'
+
+require_relative '../initializer/mongoid'
+
+require_relative 'hoge'
+require_relative 'huga'
+
+require_relative '../lib/delta_pack_builder'
+require_relative '../lib/delta_pack_unpacker'
+require_relative '../aggregators/deltas_aggregator'
 
 DatabaseCleaner.strategy = :truncation
 
